834c75f671a5150c731f428b32f5c9686d7d5625
wuihee <wuihee@gmail.com>
2024-08-30 23:18:05 +0800

    Updated frontend

---
+++ frontend/src/routes/root.jsx
@@ -0,0 +1,8 @@
+/**
+ * Renders the main page where users can search for clinical trials.
+ *
+ * @returns Component representing the main page.
+ */
+export default function Root() {
+  return <></>;
+}
