6df9abd31e2f856a42c41f6b051c4fdfa852959f
wuihee <wuihee@gmail.com>
2024-09-09 18:09:17 +0800

    Updated frontend

---
+++ frontend/src/routes/HomePage.jsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router-dom";
+
+/**
+ * Renders the main page where users can search for clinical trials.
+ */
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-zinc-100 p-5 font-serif">
+      <div className="my-5">
+        <Link to={"/"}>
+          <h1 className="text-3xl">Trial Search</h1>
+        </Link>
+        <ul>
+          <Link className="hover:underline" to={"/about"}>
+            About
+          </Link>
+        </ul>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
