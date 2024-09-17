e67c9a520532a48194b4aeb84dca0d189fc4ad54
wuihee <wuihee@gmail.com>
2024-09-16 21:14:35 -0700

    Added new page to display more detailed information about clinical trials

--- frontend/src/components/Trial.jsx
+++ frontend/src/components/Trial.jsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
+
 /**
  * Trial component visualizes a clinical trial document.
  */
 export default function Trial({ nctid, document, distance }) {
   return (
-    <div className="my-2 w-11/12 rounded-3xl bg-gray-50 p-5 hover:cursor-pointer md:w-9/12 lg:w-7/12">
+    <Link
+      className="my-2 w-11/12 rounded-3xl bg-gray-50 p-5 hover:cursor-pointer md:w-9/12 lg:w-7/12"
+      to={`/trial/${nctid}`}
+    >
       <div className="flex justify-between">
         <p className="inline font-bold">{nctid}</p>
         <p className="inline font-bold">{Math.round(distance)}</p>
       </div>
       <p>{document}</p>
-    </div>
+    </Link>
   );
 }
