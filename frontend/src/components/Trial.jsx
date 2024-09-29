2d189defd6cf0da4fcdf95c9b3f73e2de5f6fd15
wuihee <wuihee@gmail.com>
2024-09-28 22:47:08 -0700

    Added collapse & expand animation to Trial component.

--- frontend/src/components/Trial.jsx
+++ frontend/src/components/Trial.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import linkIconUrl from "../assets/link-icon.png";
 
 /**
@@ -6,6 +6,7 @@ import linkIconUrl from "../assets/link-icon.png";
  */
 export default function Trial({ nctid, metadata, distance }) {
   let [expanded, setExpanded] = useState(false);
+  let contentRef = useRef();
   let {
     brief_title: briefTitle,
     official_title: officialTitle,
@@ -26,32 +27,33 @@ export default function Trial({ nctid, metadata, distance }) {
         <p className="inline font-bold">{parseFloat(distance).toFixed(2)}</p>
       </div>
 
-      {expanded && (
-        <div>
-          <h2 className="mt-2 font-bold">Official Title</h2>
-          <p>{officialTitle}</p>
+      <div
+        ref={contentRef}
+        style={{
+          maxHeight: expanded ? `${contentRef.current.scrollHeight}px` : "0px",
+        }}
+        className="transition-max-height overflow-hidden duration-500"
+      >
+        <h2 className="mt-2 font-bold">Official Title</h2>
+        <p>{officialTitle}</p>
 
-          <h2 className="mt-2 font-bold">Brief Summary</h2>
-          <p>{briefSummary}</p>
+        <h2 className="mt-2 font-bold">Brief Summary</h2>
+        <p>{briefSummary}</p>
 
-          {detailedDescription && (
-            <h2 className="mt-2 font-bold">Detailed Description</h2>
-          )}
-          <p>{detailedDescription}</p>
+        {detailedDescription && (
+          <h2 className="mt-2 font-bold">Detailed Description</h2>
+        )}
+        <p>{detailedDescription}</p>
 
-          <h2 className="mt-2 font-bold">Eligibility Criteria</h2>
-          <p>{elgibilityCriteria}</p>
+        <h2 className="mt-2 font-bold">Eligibility Criteria</h2>
+        <p>{elgibilityCriteria}</p>
 
-          <div className="flex flex-row-reverse">
-            <a
-              href={`https://clinicaltrials.gov/study/${nctid}`}
-              target="_blank"
-            >
-              <img className="w-4" src={linkIconUrl} alt="Link Icon" />
-            </a>
-          </div>
+        <div className="flex flex-row-reverse">
+          <a href={`https://clinicaltrials.gov/study/${nctid}`} target="_blank">
+            <img className="w-4" src={linkIconUrl} alt="Link Icon" />
+          </a>
         </div>
-      )}
+      </div>
     </div>
   );
 }
